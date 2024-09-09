55a5a2ebbc12e6e3d43e47ca718a0997dfaac965
JuanGomePer <juangomeper@unisabana.edu.co>
2024-09-09 15:57:33 -0500

    add user info 2 and validation if is not a number

--- Server.js
+++ Server.js
@@ -9,19 +9,27 @@ const students = {
   1: {
     name: "Rita",
     lastName: "Trindade da Cruz",
-    email: "ritatrcr@unisabana.edu.cp",
+    email: "ritatrcr@unisabana.edu.co",
     id: "297040"
+  },
+  2: {
+    name: "Juan Andrés",
+    lastName: "Gómez Pérez",
+    email: "juangomeper@unisabana.edu.co",
+    id: "300535"
   }
-
-  //here goes the information of student 2
 };
 
 // Endpoint to get students information with the ID
 app.get('/user-info/:id', (req, res) => {
   const studentId = req.params.id;
 
+  // Validation Id is a number
+  if (!Number.isInteger(Number(studentId))) {
+    return res.status(400).json({ error: 'ID debe ser un número entero' });
+  }
   
-  // Student exist
+  // Check if the student exists in the data
   const student = students[studentId];
   if (student) {
     res.json(student);
