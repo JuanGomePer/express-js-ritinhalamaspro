494daeb1dfd112fa2b068f4a97f1dbf8cc537244
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-09-09 15:39:58 -0500

    Create server and add information student 1

---
+++ Server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Information Students
+const students = {
+  1: {
+    name: "Rita",
+    lastName: "Trindade da Cruz",
+    email: "ritatrcr@unisabana.edu.cp",
+    id: "297040"
+  }
+};
+
+// Endpoint to get students information with the ID
+app.get('/user-info/:id', (req, res) => {
+  const studentId = req.params.id;
+
+  // Validation Id is a number
+  if (!Number.isInteger(Number(studentId))) {
+    return res.status(400).json({ error: 'ID debe ser un número entero' });
+  }
+  // Verificar si el estudiante existe
+  const student = students[studentId];
+  if (student) {
+    res.json(student);
+  } else {
+    res.status(404).json({ error: 'Estudiante no encontrado' });
+  }
+});
+
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
