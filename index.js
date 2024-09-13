be27a7ade25d6ec83af37589cf0121f891ce08af
JuanGomePer <juangomeper@unisabana.edu.co>
2024-09-13 09:41:11 -0500

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-
+// Enable CORS for all routes
+app.use(cors());
 
 // Information Students
 const students = {
@@ -26,22 +28,19 @@ app.get('/user-info/:id', (req, res) => {
 
   // Validation Id is a number
   if (!Number.isInteger(Number(studentId))) {
-    return res.status(400).json({ error: 'ID debe ser un número entero' });
+    return res.status(400).json({ error: 'ID must be an integer' });
   }
-  
+
   // Check if the student exists in the data
   const student = students[studentId];
   if (student) {
     res.json(student);
   } else {
-    res.status(404).json({ error: 'Estudiante no encontrado' });
+    res.status(404).json({ error: 'Student not found' });
   }
 });
 
-
-
-
 // Start server
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
